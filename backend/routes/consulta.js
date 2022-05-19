3a5b28f15cf2cbfcb1c4d0c816cff66b03b93a5b
Daniel León Paulin <dleon29@alumnos.uaq.mx>
2022-05-19 07:34:13 -0500

    Update consulta.js

--- backend/routes/consulta.js
+++ backend/routes/consulta.js
@@ -17,6 +17,40 @@ router.get('/Diagnostico-Especifico', function (req, res, next) {
 });
 
 router.get('/Resultados', function (req, res, next) {
+
+    //Matriz Enfermedades x sintomas
+    var matriz = [];
+    //iniciar conexion
+    db = database.conectar();
+    //realizar consulta
+    db.query("SELECT * FROM Enfermedades;",
+        function (err, rows, fields) {
+            if (err) {
+                console.log(err)
+            } else {
+                for (var i = 0; i < rows.length; i++) {
+                    
+                    matriz.push([rows[i].manchasMarrones, rows[i].manchasBlancas, rows[i].manchasRojas, rows[i].descamacionDeLaPiel, rows[i].SangradoDeLaPiel, rows[i].Picazon, rows[i].Lunares, rows[i].ampollas, rows[i].hongos, rows[i].sudoracion, rows[i].inflamacionDeLaPiel, rows[i].pielSeca, rows[i].pielGangena, rows[i].granosConSebo, rows[i].ronchas]);
+                    
+                }
+                console.log(matriz)
+                // for(row in rows) {
+                //     //nombre, manchasMarrones, manchasBlancas, manchasRojas, descamacionDeLaPiel, SangradoDeLaPiel, Picazon, Lunares, ampollas, hongos, sudoracion, inflamacionDeLaPiel, pielSeca, pielGangena, granosConSebo, ronchas, descripcion, origen
+                //     //row.nombre;
+                //     console.log( row.nombre)
+                // }
+            }
+        })
+    //Terminar conexion
+    db.end(function (err) { err ? console.log(err) : console.log('Conexión terminada.'); });
+
+
+
+
+
+
+
+
     //procesar las respuestas
 
     //obtener datos de la enfermedadad con la que coincide
@@ -40,7 +74,7 @@ router.get('/Resultados', function (req, res, next) {
                             console.log(err)
                         } else {
                             res.status(200);
-                            res.json({"Enfermedad":results[0], "Recomendaciones": results2});
+                            res.json({ "Enfermedad": results[0], "Recomendaciones": results2 });
                         }
                     })
                 //Terminar conexion
