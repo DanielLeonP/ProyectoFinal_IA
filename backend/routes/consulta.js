b7ad56c5a62f89180bece8eed61c15677da6edba
Daniel León Paulin <dleon29@alumnos.uaq.mx>
2022-05-19 08:35:30 -0500

    add

--- backend/routes/consulta.js
+++ backend/routes/consulta.js
@@ -19,7 +19,7 @@ router.get('/Diagnostico-Especifico', function (req, res, next) {
 router.get('/Resultados', function (req, res, next) {
 
     //Matriz Enfermedades x sintomas
-    var matriz = [];
+    let matriz = [];
     //iniciar conexion
     db = database.conectar();
     //realizar consulta
@@ -29,24 +29,51 @@ router.get('/Resultados', function (req, res, next) {
                 console.log(err)
             } else {
                 for (var i = 0; i < rows.length; i++) {
-                    
                     matriz.push([rows[i].manchasMarrones, rows[i].manchasBlancas, rows[i].manchasRojas, rows[i].descamacionDeLaPiel, rows[i].SangradoDeLaPiel, rows[i].Picazon, rows[i].Lunares, rows[i].ampollas, rows[i].hongos, rows[i].sudoracion, rows[i].inflamacionDeLaPiel, rows[i].pielSeca, rows[i].pielGangena, rows[i].granosConSebo, rows[i].ronchas]);
-                    
                 }
                 console.log(matriz)
-                // for(row in rows) {
-                //     //nombre, manchasMarrones, manchasBlancas, manchasRojas, descamacionDeLaPiel, SangradoDeLaPiel, Picazon, Lunares, ampollas, hongos, sudoracion, inflamacionDeLaPiel, pielSeca, pielGangena, granosConSebo, ronchas, descripcion, origen
-                //     //row.nombre;
-                //     console.log( row.nombre)
-                // }
+
+
+
+
+
+
+
+                const respuestaUsuario = [0, 0.3, 0.5, 0.4, 08, 0.9, 0.4, 0.0, 0.7, 0.1, 0.1, 0.6, 0.2, 0.0, 0.5];
+
+                const umbral = 0.3;
+                //procesar
+
+                // Cada enfermedad    
+                //Aplicar Max-Min
+                let min = [];
+                let enfermedad= [];
+                let temporal = [];
+                for (let i = 0; i < matriz.length; i++) {
+                    console.log("enfermedad " + (i+1))
+                    enfermedad= matriz[i];
+                    
+                    for (let j = 0; j < enfermedad.length; j++) {
+                        temporal[j] = Math.min(respuestaUsuario[j],enfermedad[j])
+                        //console.log(temporal[j])
+
+                    } 
+                    min[i]= temporal;
+                    temporal = [];
+                    min
+                    console.log("El minimo en "+  (i+1) +" es " + min[i])
+                }
+                
+
+
+
+
+
+
             }
         })
     //Terminar conexion
     db.end(function (err) { err ? console.log(err) : console.log('Conexión terminada.'); });
-
-
-
-
 
 
 
